ad903f93911e8af0a22dd3b2c43aef50e55df7ce
Pano Maria <pano.maria@gmail.com>
2026-03-24 15:06:45 +0100

    Versie naar vastgesteld

--- js/config.js
+++ js/config.js
@@ -9,8 +9,8 @@ let respecConfig = {
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
   //specStatus: "wv",                   // Werkversie
   // specStatus: "cv",               // Consultatieversie
-  specStatus: "vv",               // Versie ter vaststelling
-  //specStatus: "def",              // Vastgestelde versie
+  // specStatus: "vv",               // Versie ter vaststelling
+  specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
   
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
